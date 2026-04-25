c6bdc641bf1ac2e6accb617c88119e6e245573e7
abdulmazidkn <abdulmazidkhangm@gmail.com>
2026-04-25 10:32:32 +0600

    Create Returning Objects from Functions (Complex).cpp

---
+++ KFI/Returning Objects from Functions (Complex).cpp
@@ -0,0 +1,43 @@
+This snippet demonstrates how a friend function can return a new object (a Complex sum).
+#include <iostream>
+using namespace std;
+
+class Complex{
+    float x;
+    float y;
+
+    public:
+    void input(float real, float imag)
+    {
+        x = real;
+        y = imag;
+    }
+    friend Complex sum(Complex c1, Complex c2);
+    void show(Complex);
+};
+
+Complex sum(Complex c1, Complex c2)
+{
+    Complex c3;
+    c3.x = c1.x + c2.x;
+    c3.y = c1.y + c2.y;
+    return c3;
+}
+
+void Complex :: show(Complex c)
+{
+    cout << c.x << "+j" << c.y << endl;
+}
+
+int main()
+{
+    Complex A, B, C;
+    A.input(3.1, 5.65);
+    B.input(2.75, 1.2);
+    C = sum(A, B);
+
+    cout << "A = "; A.show(A);
+    cout << "B = "; B.show(B);
+    cout << "C = "; C.show(C);
+    return 0;
+}
